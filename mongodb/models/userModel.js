f54f97563c500ae088c8710db4478278171babce
Ahmad Sabbah <ahmadsb@post.jce.ac.il>
2019-08-07 22:01:22 +0300

    resetPassword

--- mongodb/models/userModel.js
+++ mongodb/models/userModel.js
@@ -86,7 +86,6 @@ userSchema.methods.createPasswordRestToken = function(){
         .createHash('sha256')
         .update(resetToken)
         .digest('hex');
-    console.log({resetToken}, this.passwordResetToken);
     this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
     return resetToken;
